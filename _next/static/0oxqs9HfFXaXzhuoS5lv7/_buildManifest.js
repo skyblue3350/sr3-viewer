8ca311c0a95d050a6e9d5f151fd31c059f8d4b16
skyblue3350 <skyblue3350@users.noreply.github.com>
2023-01-28 17:10:28 +0000

    deploy: 63b591774d86acd9267b7316fb09609d692d931b

---
+++ _next/static/0oxqs9HfFXaXzhuoS5lv7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/752-3c050a6067a92a75.js","static/chunks/pages/index-509caa755a3c0256.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
